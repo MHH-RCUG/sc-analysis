2e4f5a8a52ea8dcc02394a5f13a860fea077e057
kosankem <kosanke.maike@mh-hannover.de>
2024-02-26 15:56:17 +0100

    Correction bioconductor version

--- modules/download_test_datasets/installation_download_test_datasets.R
+++ modules/download_test_datasets/installation_download_test_datasets.R
@@ -1,6 +1,6 @@
 # Setting up download_test_datasets renv 
 # in /modules/download_test_datasets
-renv::init(bioconductor = TRUE, bare = TRUE) # bioconductor version 1.6.3 for R 4.2
+renv::init(bioconductor = TRUE, bare = TRUE) # bioconductor version 3.16 for R 4.2
 renv::install("Seurat") # version 5.0.1
 renv::install("openxlsx") # version 4.2.5.2
 renv::install("R.utils") # version 2.12.3
