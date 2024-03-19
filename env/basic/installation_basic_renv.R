e2faf576b624d3114834c1de60f7d5784dfba91f
kosankem <kosanke.maike@mh-hannover.de>
2024-03-19 15:35:34 +0100

    Update renv and include python env

--- env/basic/installation_basic_renv.R
+++ env/basic/installation_basic_renv.R
@@ -1,6 +1,6 @@
-# Setting up download_test_datasets renv 
-# in /modules/download_test_datasets
+# Set renv
 renv::init(bioconductor = TRUE, bare = TRUE) # bioconductor version 3.16 for R 4.2
+# Project '/mnt/ngsnfs/single_cell_dev/scRNAseq_processing/sc_analysis/env/basic' [renv 0.16.0]
 
 renv::install("remotes")
 renv::install("tidyverse") # version 2.0.0; dbplyr version < "2.3.4" needed
@@ -19,7 +19,30 @@ renv::install("bioc::scran") # version 1.26.2
 renv::install("viridis") # version 0.6.5
 renv::install("pheatmap") # version 1.0.12
 renv::install("sessioninfo") # version 1.2.2
+renv::install("markdown") # version 1.12
+remotes::install_version("xfun", version="0.41")
+renv::install("knitcitations") # version 1.0.12
 
+# Further packages:
+# reticulate 1.35.0
+# knitr 1.45
+# rmarkdown 2.25
+# htmltools 0.5.7
+# httpuv 1.6.14
+# httr 1.4.7
+# bslib 0.6.1
+# shiny 1.8.0
 
+# Set virtualenv
+reticulate::virtualenv_create("/mnt/ngsnfs/single_cell_dev/scRNAseq_processing/sc_analysis/env/basic/virtualenvs/r-reticulate")
+renv::use_python(type = "virtualenv", name = "/mnt/ngsnfs/single_cell_dev/scRNAseq_processing/sc_analysis/env/basic/virtualenvs/r-reticulate")
+# Python 3.8.10 in virtualenv; ./virtualenvs/r-reticulate
+# In Project '/mnt/ngsnfs/single_cell_dev/scRNAseq_processing/sc_analysis/env/basic' [renv 0.16.0]
+
+#use_virtualenv("/mnt/ngsnfs/single_cell_dev/scRNAseq_processing/sc_analysis/env/basic/virtualenvs/r-reticulate")
+
+reticulate::py_install("pandas", envname = "/mnt/ngsnfs/single_cell_dev/scRNAseq_processing/sc_analysis/env/basic/virtualenvs/r-reticulate")
+reticulate::py_install("leidenalg", envname = "/mnt/ngsnfs/single_cell_dev/scRNAseq_processing/sc_analysis/env/basic/virtualenvs/r-reticulate")
+reticulate::py_install("anndata", envname = "/mnt/ngsnfs/single_cell_dev/scRNAseq_processing/sc_analysis/env/basic/virtualenvs/r-reticulate")
 
 
