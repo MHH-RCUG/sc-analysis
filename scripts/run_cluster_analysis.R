904ac3389abeb90112f654b4b7c81741955dfaab
kosankem <kosanke.maike@mh-hannover.de>
2024-09-02 15:29:27 +0200

    Modify loading of parameter settings in start scripts

--- scripts/run_cluster_analysis.R
+++ scripts/run_cluster_analysis.R
@@ -4,52 +4,20 @@ param=list()
 
 # Set paths
 param$path_to_git='/mnt/ngsnfs/single_cell_dev/scRNAseq_processing/sc_analysis'
+param$path_to_basic_settings=file.path(param$path_to_git,"basic_settings.R")
+param$path_to_advanced_settings=file.path(param$path_to_git,"advanced_settings.R")
 setwd(param$path_to_git)
 param$scriptname = "scripts/pre-processing/cluster_analysis.Rmd"
 
 # Set environment
+renv::use_python(type = "virtualenv", name = file.path(param$path_to_git,"env/basic/virtualenvs/r-reticulate"))
 renv::load(file.path(param$path_to_git,"env/basic"))
 
-# Set standard parameter
-source(file.path(param$path_to_git,"config/set_standard_parameter.R"))
-
-
-
-### Parameter
-################################################################################
-
-########## Basic settings ########## 
-
-# Set project name
-param$project_id = "Testdata"
-
-# Select data  
-# Load existing object
-param$data = "/mnt/ngsnfs/single_cell_dev/scRNAseq_processing/sc_analysis/output/Testdata/pre-processing/data/sc.rds"
-
-# Reference
-param$species="human"
-
-
-
-########## Advanced settings ########## 
-
-# Set advanced parameter
-source(file.path(param$path_to_git,"/advanced_settings.R"))
-
+# Set parameter
+source(file.path(param$path_to_git,"config/set_parameter.R"))
 
 
 
 ### Run markdown
 ################################################################################
-
-# Set output folder
-param$path_out = file.path(param$path_to_git, "output", param$project_id, gsub(".Rmd", "", basename(param$scriptname)))
-
-# Record advanced parameter settings
-param = modifyList(x = param, val = param_advset)
-
-# Create output directories
-if (!file.exists(param$path_out)) dir.create(param$path_out, recursive=TRUE, showWarnings=FALSE)
-# Run markdown
 rmarkdown::render(file.path(param$path_to_git,param$scriptname), param = param, output_file = file.path(param$path_out, paste0(param$project_id,"_", gsub("Rmd", "html", basename(param$scriptname)))))
--- scripts/run_pre-processing.R
+++ scripts/run_pre-processing.R
@@ -4,78 +4,20 @@ param=list()
 
 # Set paths
 param$path_to_git='/mnt/ngsnfs/single_cell_dev/scRNAseq_processing/sc_analysis'
+param$path_to_basic_settings=file.path(param$path_to_git,"basic_settings.R")
+param$path_to_advanced_settings=file.path(param$path_to_git,"advanced_settings.R")
 setwd(param$path_to_git)
 param$scriptname = "scripts/pre-processing/pre-processing.Rmd"
 
 # Set environment
+renv::use_python(type = "virtualenv", name = file.path(param$path_to_git,"env/basic/virtualenvs/r-reticulate"))
 renv::load(file.path(param$path_to_git,"env/basic"))
 
-# Set standard parameter
-source(file.path(param$path_to_git,"config/set_standard_parameter.R"))
-
-
-
-### Parameter
-################################################################################
-
-########## Basic settings ########## 
-
-# Set project name
-param$project_id = "Testdata"
-
-# Select data  
-  # Either load existing object
-    param$data = NULL
-    #param$data = "/mnt/ngsnfs/single_cell_dev/scRNAseq_processing/sc_analysis/data/BGHFO/sc.rds"
-  # Or use test dataset
-    # One of the datasets included in ./modules/download_test_datasets/
-    # Does only work for 10x data automatically at the moment
-    param$download_test_datasets=NULL
-    #param_advset$download_test_datasets="download_10x_pbmc_small_split2samples"
-  # Or set data path
-    #param$path_data = NULL
-    param$path_data = data.frame(name=c("sample1", "sample2"),  
-                                 type=c("10x"), 
-                                 path=c("/mnt/ngsnfs/single_cell_dev/scRNAseq_processing/sc_analysis/data/counts/sample1/",
-                                        "/mnt/ngsnfs/single_cell_dev/scRNAseq_processing/sc_analysis/data/counts/sample2/"))
-
-# Reference
-param$species="human"
-
-
-
-########## Advanced settings ########## 
-
-# Set advanced parameter
-source(file.path(param$path_to_git,"/advanced_settings.R"))
-
-# Filter for cells
-param_advset$cell_filter = list(nFeature_RNA=c(500, 2400), nCount_RNA=c(NA, NA), percent_mt=c(NA, 20))
-
-# Downsample all samples equally according to the smallest sample
-#param$downsample_cells_equally = TRUE
-
-# Normalization
-#param_advset$norm="SCT"
-
-# Dimensional reduction
-#param_advset$pc_n = 10
-
-# How to combine multiple datasets
-#param_advset$integrate_samples = list(method="integrate")
+# Set parameter
+source(file.path(param$path_to_git,"config/set_parameter.R"))
 
 
 
 ### Run markdown
 ################################################################################
-
-# Set output folder
-param$path_out = file.path(param$path_to_git, "output", param$project_id, gsub(".Rmd", "", basename(param$scriptname)))
-
-# Record advanced parameter settings
-param = modifyList(x = param, val = param_advset)
-
-# Create output directories
-if (!file.exists(param$path_out)) dir.create(param$path_out, recursive=TRUE, showWarnings=FALSE)
-# Run markdown
 rmarkdown::render(file.path(param$path_to_git,param$scriptname), param = param, output_file = file.path(param$path_out, paste0(param$project_id,"_", gsub("Rmd", "html", basename(param$scriptname)))))
