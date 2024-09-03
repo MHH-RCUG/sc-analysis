ee074b0b688e1499cc9780c8177d23cdeb99aacd
kosankem <kosanke.maike@mh-hannover.de>
2024-09-03 16:37:52 +0200

    Fix problem with anndata export

--- scripts/export_data/convert.R
+++ scripts/export_data/convert.R
@@ -21,16 +21,19 @@ if (all(param$path_data$type == "10x")) {
 
 ### Export as andata object (h5ad file)
 # Convert Seurat v5 single cell object to anndata object
-scCustomize::as.anndata(x = sc, file_path = file.path(param$path_out, "data"), file_name = "sc_anndata.h5ad", )
+# Does not work for SCT at the moment
+# However, it would contain counts and data layer
+#scCustomize::as.anndata(x = sc, file_path = file.path(param$path_out, "data"), file_name = "sc_anndata.h5ad", assay="RNA", 
+#                        main_layer = "data", other_layers = "counts", transer_dimreduc = TRUE, verbose = FALSE)
 
 # scesy only worked for v3 and v4 objects
-# convert to v3
-#sc_v3 = sc
-#sc_v3[["RNA"]] = as(sc_v3[["RNA"]], "Assay")
+# Convert to V3/4/Assay structure
+sc_v3 = scCustomize::Convert_Assay(seurat_object = sc, convert_to = "V3", assay = "RNA")
+
 # Convert Seurat v3 single cell object to anndata object
-#adata = sceasy::convertFormat(sc_v3, from="seurat", to="anndata", outFile=NULL, assay=DefaultAssay(sc))
+adata = sceasy::convertFormat(sc_v3, from="seurat", to="anndata", outFile=NULL, assay=DefaultAssay(sc))
 # Write to h5ad file
-#adata$write(file.path(param$path_out, "data", "sc.h5ad"), compression="gzip")
+adata$write(file.path(param$path_out, "data", "sc_anndata.h5ad"), compression="gzip")
 
 
 
