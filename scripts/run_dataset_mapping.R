35698eb3eac95ac9dca398b469a2bddb2e577cd9
kosankem <kosanke.maike@mh-hannover.de>
2024-09-04 14:33:10 +0200

    Dataset mapping with output and modified parameter setting

--- scripts/run_dataset_mapping.R
+++ scripts/run_dataset_mapping.R
@@ -8,70 +8,22 @@
 ################################################################################
 param=list()
 
-# set paths
+# Set paths
 param$path_to_git='/mnt/ngsnfs/single_cell_dev/scRNAseq_processing/sc_analysis'
+param$path_to_basic_settings=file.path(param$path_to_git,"basic_settings.R")
+param$path_to_advanced_settings=file.path(param$path_to_git,"advanced_settings.R")
 setwd(param$path_to_git)
 param$scriptname = "scripts/dataset_mapping/dataset_mapping_seurat.Rmd"
 
-# set environment
+# Set environment
+renv::use_python(type = "virtualenv", name = file.path(param$path_to_git,"env/basic/virtualenvs/r-reticulate"))
 renv::load(file.path(param$path_to_git,"env/basic"))
 
-# set standard parameter
-source(file.path(param$path_to_git,"config/set_standard_parameter.R"))
-
-
-
-### Parameter
-################################################################################
-
-########## Basic settings ########## 
-
-# set project name
-param$project_id = "BG-HFO_vs_HFO"
-
-# set reference object
-param$refdata = "/mnt/ngsnfs/single_cell_dev/scRNAseq_processing/sc_analysis/data/HFO/sc.rds"
-# set query object
-param$data = "/mnt/ngsnfs/single_cell_dev/scRNAseq_processing/sc_analysis/data/BGHFO/sc.rds"
-
-# Pre-annotated cell types; column in reference dataset
-param$celltype = "annotation"   # at the moment required
-param$reduction = 'umap'
-param$predicted_score_threshold = 0.9
-param$percent_predicted_cells_threshold = 0.1
-
-
-
-########## Advanced settings ########## 
-
-# Set advanced parameter
-source(file.path(param$path_to_git,"/advanced_settings.R"))
-
-# Set colors
-#param_advset$col_annotation_ref = c('Cluster 1-CM' = "#99e600FF", 'Cluster 3-Mesenchyme' = "#99e6e6FF", 'Cluster 4-Liver' = "#e6b800FF",
-#                             'Cluster 6-Apoptotic' = "#000000FF", 'Cluster 2-AFE' = "#99BBFFFF", 'Cluster 7-ECs-Blood' = "#ffff1aFF", 
-#                             'Cluster 5-hPSCs' = "#CFCFCFFF")
-#param_advset$col_annotation = c('EPP' = "#4682B4FF", 'VE' = "#E6CCFFFF", 'PE/ST' = "#EEC900FF", 'MES' = "#CFCFCFFF", 
-#                             'AE' = "#FFB3D9FF", 'MKs' = "#80FFE5FF", 'PFE' = "#80FFE5FF", 'CM' = "#EE2C2CFF",
-#                             'Ery' = "#B33C00FF", 'Mo/Mo'	= "#99BBFFFF", 'EC' = "#CD00CDFF", 'HPC' = "#FFCC99FF",
-#                             'ELC' = "#EE2C2CFF")
-
-# compute_combined_umap_projection parameters
-#param_advset$pc_n = 13
-#param_advset$umap_k = 30
+# Set parameter
+source(file.path(param$path_to_git,"config/set_parameter.R"))
 
 
 
 ### Run markdown
 ################################################################################
-
-# Set output folder
-param$path_out = file.path(param$path_to_git, "output", param$project_id, gsub(".Rmd", "", basename(param$scriptname)))
-
-# Record advanced parameter settings
-param = modifyList(x = param, val = param_advset)
-
-# Create output directories
-if (!file.exists(param$path_out)) dir.create(param$path_out, recursive=TRUE, showWarnings=FALSE)
-# Run markdown
 rmarkdown::render(file.path(param$path_to_git,param$scriptname), param = param, output_file = file.path(param$path_out, paste0(param$project_id,"_", gsub("Rmd", "html", basename(param$scriptname)))))
