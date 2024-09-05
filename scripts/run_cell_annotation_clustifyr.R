0c649de344d8e94032b41d92cba6d99ca37a9737
kosankem <kosanke.maike@mh-hannover.de>
2024-09-05 18:16:20 +0200

    Cluster annotation via xlustifyr

---
+++ scripts/run_cell_annotation_clustifyr.R
@@ -0,0 +1,29 @@
+### Requirements
+################################################################################
+# Both datasets (reference and query) must be Seurat objects (rds files) with a @meta.data$annotation annotation column with a cell type annotation or at least a cluster number!
+
+
+
+### Configuration
+################################################################################
+param=list()
+
+# Set paths
+param$path_to_git='/mnt/ngsnfs/single_cell_dev/scRNAseq_processing/sc_analysis'
+param$path_to_basic_settings=file.path(param$path_to_git,"basic_settings.R")
+param$path_to_advanced_settings=file.path(param$path_to_git,"advanced_settings.R")
+setwd(param$path_to_git)
+param$scriptname = "scripts/dataset_mapping/cell_annotation_clustifyr.Rmd"
+
+# Set environment
+renv::use_python(type = "virtualenv", name = file.path(param$path_to_git,"env/basic/virtualenvs/r-reticulate"))
+renv::load(file.path(param$path_to_git,"env/basic"))
+
+# Set parameter
+source(file.path(param$path_to_git,"config/set_parameter.R"))
+
+
+
+### Run markdown
+################################################################################
+rmarkdown::render(file.path(param$path_to_git,param$scriptname), param = param, output_file = file.path(param$path_out, paste0(param$project_id,"_", gsub("Rmd", "html", basename(param$scriptname)))))
