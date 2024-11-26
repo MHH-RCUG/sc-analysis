7d839bf576a765b9f9a8e8e6c587e57d81bc863f
kosankem <kosanke.maike@mh-hannover.de>
2024-11-26 16:36:11 +0100

    Include configuration.R in start settings

--- config/start_settings.R
+++ config/start_settings.R
@@ -7,3 +7,6 @@ param$path_to_git='/mnt/ngsnfs/single_cell_dev/scRNAseq_processing/sc_analysis'
 param$path_to_basic_settings=file.path(param$path_to_git,"basic_settings.R")
 param$path_to_advanced_settings=file.path(param$path_to_git,"advanced_settings.R")
 setwd(param$path_to_git)
+
+# set configuration
+source(file.path(param$path_to_git,'config/configuration.R'))
--- scripts/manual_start.R
+++ scripts/manual_start.R
@@ -4,6 +4,8 @@ param=list()
 
 # set paths
 param$path_to_git='/mnt/ngsnfs/single_cell_dev/scRNAseq_processing/sc_analysis'
+param$path_to_basic_settings=file.path(param$path_to_git,"basic_settings.R")
+param$path_to_advanced_settings=file.path(param$path_to_git,"advanced_settings.R")
 setwd(param$path_to_git)
 
 # set environment (one of both)
@@ -11,3 +13,6 @@ setwd(param$path_to_git)
 renv::activate(file.path(param$path_to_git,"env/basic"))
 # ccc renv
 renv::activate(file.path(param$path_to_git,"env/ccc"))
+
+# set configuration
+source(file.path(param$path_to_git,'config/configuration.R'))
