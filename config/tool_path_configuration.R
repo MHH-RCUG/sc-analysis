0fed7c3de4765a4f059127c8942e4fb755052cd1
kosankem <kosanke.maike@mh-hannover.de>
2024-12-02 18:13:16 +0100

    Add a access point for main path configuration, i.e. definition

---
+++ config/tool_path_configuration.R
@@ -0,0 +1,12 @@
+### Configuration
+################################################################################
+# Tool path
+path_to_scanalysis = '/mnt/ngsnfs/single_cell_dev/scRNAseq_processing/sc_analysis'
+
+# Path to list with parameter settings
+# Needed for running sc-analysis from RStudio or command line 
+# Basic settings (default: basic_settings.R in path_to_scanalysis)
+path_to_basic_settings = NULL
+# Advanced settings (default: advanced_settings.R in path_to_scanalysis)
+path_to_advanced_settings = NULL
+
