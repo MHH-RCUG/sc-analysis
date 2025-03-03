75a5b511ef61aef328198731771f1829441848fe
kosankem <kosanke.maike@mh-hannover.de>
2025-03-03 18:10:29 +0100

    Correct path

--- config/start_settings.R
+++ config/start_settings.R
@@ -6,7 +6,7 @@ param=list()
 source("config/tool_path_configuration.R")
 param$path_to_git=path_to_scanalysis
 if (is.null(path_to_basic_settings)) {param$path_to_basic_settings=file.path(param$path_to_git,"basic_settings.R")}
-if (is.null(path_to_advanced_settings)) {param$path_to_basic_settings=file.path(param$path_to_git,"advanced_settings.R")}
+if (is.null(path_to_advanced_settings)) {param$path_to_advanced_settings=file.path(param$path_to_git,"advanced_settings.R")}
 if (getwd()!=param$path_to_git) {
   setwd(param$path_to_git)
 }
